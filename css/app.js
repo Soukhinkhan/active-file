79d6082c19cc3cc2ee36c49999052b3ae810dcd2
Soukhin Khan <110587191+Soukhinkhan@users.noreply.github.com>
2022-12-10 11:23:59 +0600

    Add files via upload

---
+++ css/app.js
@@ -0,0 +1,15 @@
+$('.banner-slider').owlCarousel({
+    loop:true,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
